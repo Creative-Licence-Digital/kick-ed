8393f8f241b556739be418b8963f8f570fd57593
MathieuGuillout <mathieu@creativelicence.com.au>
2016-08-02 14:36:02 -0400

    Refactor to move methods on the API controller

--- src/submissions.js
+++ src/submissions.js
@@ -2,14 +2,69 @@ import Models from './models'
 
 export default (config) => {
   const models  = Models(config);
-  let   methods = {};
 
-  methods.list = (where) => {
+  // Returns an instance with default values 
+  const defaultInstance = () => models.UserContent.build();
+
+  // List all instances for a certain query
+  const list = (where) => {
     return models.UserContent.findAll({
       where:   where,
       include: [ models.UserContentField ]
     });
   }
 
-  return methods;
+  // Get a submission by uuid
+  const getByUuid = (uuid) => {
+    return models.UserContent.findOne({
+      where:   { uuid },
+      include: [ models.UserContentField ]
+    });
+  }
+
+  // All submissions posted by an user
+  const allContentForUser = (user) => models.UserContent.findAll({
+    where:   { user },
+    include: [ models.UserContentField ]
+  });
+
+  // Update a User Content Field in the DB (Or creates it if needed)
+   const updateField = (field) => {
+    return models.UserContentField.upsert(field)
+  }
+
+  // Update or create a submission
+  const update = (uc) => new Promise((resolve, reject) => {
+    // Link the fields to the user content, and stores their position
+    if (uc.openedAt) {
+      uc.openedAt = new Date(uc.openedAt);
+    }
+
+    if (uc.submittedAt) {
+      uc.submittedAt = new Date(uc.submittedAt);
+    }
+    const fields = (uc.fields || []).map((f, i) => {
+      f.UserContentUuid = uc.uuid; 
+      f.position = i + 1;
+      return f;
+    });
+    delete uc.fields;
+    const createUC        = models.UserContent.upsert(uc);
+
+    createUC.then(() => {
+      const updateAllFields = fields.map(updateField);
+      Promise.all(updateAllFields)
+             .then(() => resolve())
+             .catch(reject);
+
+    }).catch(reject);
+  });
+
+
+  return { list, 
+           getByUuid, 
+           updateField, 
+           update, 
+           defaultInstance,
+           allContentForUser };
 }
--- src/syncRouter.js
+++ src/syncRouter.js
@@ -1,47 +1,7 @@
-import Models from './models'
-
+import Submissions from './submissions'
 
 export default (app, config) => {
-  const models = Models(config);
-
-  // Update a User Content Field in the DB (Or creates it if needed)
-  const updateField = (field) => {
-    return models.UserContentField.upsert(field)
-  }
-
-  // Update User Content in the DB 
-  // Updates also all the related fields
-  const updateUC = (uc) => new Promise((resolve, reject) => {
-    // Link the fields to the user content, and stores their position
-    console.error("UC", uc.openedAt, typeof uc.openedAt);
-    if (uc.openedAt) {
-      uc.openedAt = new Date(uc.openedAt);
-    }
-
-    if (uc.submittedAt) {
-      uc.submittedAt = new Date(uc.submittedAt);
-    }
-    const fields = (uc.fields || []).map((f, i) => {
-      f.UserContentUuid = uc.uuid; 
-      f.position = i + 1;
-      return f;
-    });
-    delete uc.fields;
-    const createUC        = models.UserContent.upsert(uc);
-
-    createUC.then(() => {
-      const updateAllFields = fields.map(updateField);
-      Promise.all(updateAllFields)
-             .then(() => resolve())
-             .catch(reject);
-
-    }).catch(reject);
-  });
-
-  const allContentForUser = (user) => models.UserContent.findAll({
-    where:   { user },
-    include: [ models.UserContentField ]
-  });
+  const submissions = Submissions(config);
 
   app.post("/api/sync", (req, res, next) => {
     const user = req.body["user_data"].userData._id.toString();
@@ -50,16 +10,14 @@ export default (app, config) => {
       return uc;
     });
 
-    Promise.all(ucs.map(updateUC))
+    Promise.all(ucs.map(submissions.update))
            .then(() => {
-              allContentForUser(user).then((ucs) => {
+              submissions.allContentForUser(user).then((ucs) => {
                 req.body.user_content = ucs.map(uc => uc.toJSON());
                 next();
               });
             })
-           .catch((e) => {
-              console.error(e) 
-           });
+           .catch((e) => console.error(e));
   });
 
 }
